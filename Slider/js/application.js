6820e674aadb6d223fe0399bc510f357cc30423b
Lê Hoàn Chân <Le.Hoan.Chan@gmail.com>
2019-03-16 13:07:54 +0700

    Thêm Khả năng xử lý video và link

--- Slider/js/application.js
+++ Slider/js/application.js
@@ -1,28 +1,36 @@
 "use strict";
-$(document).ready(function() {
+$(document).ready(function () {
   function showText() {
     var args = arguments;
     var text = args[0];
     for (var key = 1; key < args.length; key++)
-        text = text.replace(new RegExp('\\{' + (key - 1) + '\\}', 'g'), args[key]);
+      text = text.replace(new RegExp('\\{' + (key - 1) + '\\}', 'g'), args[key]);
     return text;
   }
+
   function CreateHTML(data) {
     if ($.isEmptyObject(data)) return "";
     var text = '';
-    data.forEach(function(p) {
-      text += showText(
-          '<li>' +
-              (p.Video?'<video controls autoplay src="' + p.Video + '">video not supported</video>'
-                :'<img src="' + p.Image + '" alt="{0}">') +
-              (p.Position ? 
-              '<div class="container {2}">' +
-                '<h1 class="header">{0}</h1>' +
-                '<div class="content">' +
-                  '<p>{1}</p>' +
-                '</div>' +
-              '</div>' : "") +
-            '</li>', p.Header, p.Content, p.Position);
+    data.forEach(function (p) {
+      var content = "";
+      if (p.Video) {
+        content = '<li data-type="video"><video controls autoplay src="' + p.Video + '">video not supported</video>';
+      }
+      else if (p.Link) {
+        content = '<li data-type="link"><a href="' + p.Link + '" title="{0}">{0}</a>';
+      }
+      else if (p.Image) {
+        content = '<li data-type="image"><img src="' + p.Image + '" alt="{0}">';
+      }
+      text += showText( content +
+        (p.Position ?
+          '<div class="container {2}">' +
+          '<h1 class="header">{0}</h1>' +
+          '<div class="content">' +
+          '<p>{1}</p>' +
+          '</div>' +
+          '</div>' : "") +
+        '</li>', p.Header, p.Content, p.Position);
     });
     return text;
   }
@@ -39,11 +47,12 @@ $(document).ready(function() {
     scrollable: true
   });
   var audio = $('#music').get(0);
-  audio.play();
-  audio.ended = function(){
-    audio.load();
-    audio.currentTime = 0;
+  if (audio) {
     audio.play();
-  };
-  
+    audio.ended = function () {
+      audio.load();
+      audio.currentTime = 0;
+      audio.play();
+    };
+  }
 });
